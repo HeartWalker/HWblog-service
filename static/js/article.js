4ae8d9a3e595522b3b30a9d6982aca194e498ca0
zhangtonglei <zhangtonglei@tidebuy.net>
2017-10-08 18:02:53 +0800

    1008

---
+++ static/js/article.js
@@ -0,0 +1,36 @@
+
+document.getElementById("upImgClick").addEventListener('click',function (e) {
+    if(!document.getElementById("img").value){
+        return false;
+    }
+    var form = document.getElementById("upImg");
+    if(!document.getElementById("formIframe")){
+        var iframe = document.createElement("iframe");
+        iframe.name = form.target;
+        iframe.style.display = "none";
+        form.appendChild(iframe);
+    }
+    iframe.onload = function(){
+        var responseData = this.contentDocument.body.textContent
+            || this.contentWindow.document.body.textContent;
+        warnLayer(responseData);
+        // console.log(responseData);
+    };
+    form.submit();
+    form.reset();
+});
+
+document.getElementById('textSubmit').addEventListener('click',function (event) {
+    var content = document.getElementById('content').value,
+        title = document.getElementById('title').value;
+    if(!title){
+        warnLayer('标题不能为空');
+        event.preventDefault();
+
+    }else if(!content){
+        warnLayer('内容不能为空');
+        event.preventDefault();
+    }
+
+
+});
--- static/js/warnLayer.js
+++ static/js/warnLayer.js
@@ -1,5 +1,5 @@
 ;
-(function warnLayer(layerId,message) {
+function warnLayer(layerId,message) {
     var id = layerId;
     var msg = message;
     var timer = null;
@@ -8,20 +8,22 @@
         id = 'warnLayer';
         msg = layerId || '';
     }
-    if(!document.getElementById(id)){
-        var layer = document.createElement('div');
-        layer.id = id;
-        layer.innerHTML = msg;
-        layer.style = "display:block;position:fixed; top:20%;margin:0 auto;" +
-            "text-align:'center';font-size:26px;color:'orange';";
-        document.body.appendChild(layer);
-        var timer = setTimeout(function () {
+    var layer = document.getElementById(id) || document.createElement('div');
+    timer = function () {
+        setTimeout(function () {
             layer.style.display = 'none';
         },2000);
-        timer();
-
     }
     layer.innerHTML = msg;
+    if(!document.getElementById(id)){
+        layer.id = id;
+        layer.style = "display:block;position:fixed; top:20%;transform:translatex(-50%);left:50%;" +
+            "text-align:'center';font-size:42px;color:orange;";
+        document.body.appendChild(layer);
+
+    }
+    layer.style.display = 'block'
     timer();
-    return warnLayer;
-})()
+
+
+}
